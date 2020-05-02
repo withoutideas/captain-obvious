6131f9e60d52510e4f9f323ad732f23bfa19836b
alex <lazywebstudying@gmail.com>
2020-05-02 18:54:22 +0300

    mmmm

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-const menu_toggle = document.querySelector(".header__menu");
+//const menu_toggle = document.querySelector(".header__menu");
 //const nav_position = document.querySelector(".nav");
 
-menu_toggle.onclick = function() {
+document.querySelector(".header__menu").onclick = function() {
     document.querySelector(".menu-button").classList.toggle("menu-button_active");
     document.querySelector(".navigation").classList.toggle("navigation_active");
     document.querySelector(".page").classList.toggle("page_adroop");
+}
+
+document.querySelector(".navigation__item").onclick = function() {
 }
