1029b2a071976dd869401fc5b9bebe4a484a6641
alex <lazywebstudying@gmail.com>
2020-05-15 01:38:24 +0300

    набросок мобильной навигации

--- js/main.js
+++ js/main.js
@@ -3,9 +3,27 @@
 
 document.querySelector(".header__menu").onclick = function() {
     document.querySelector(".menu-button").classList.toggle("menu-button_active");
-    document.querySelector(".navigation").classList.toggle("navigation_active");
-    document.querySelector(".page").classList.toggle("page_adroop");
+    document.querySelector(".nav").classList.toggle("nav_active");
+    
+    document.getElementById("button_categories").classList.remove("nav-button_active");
+    document.getElementById("item_categories").classList.remove("nav__item_active");
+
+    document.getElementById("button_authors").classList.remove("nav-button_active");
+    document.getElementById("item_authors").classList.remove("nav__item_active");
 }
 
-document.querySelector(".navigation__item").onclick = function() {
+document.getElementById("nav__button_categories").onclick = function() {
+    document.getElementById("button_categories").classList.toggle("nav-button_active");
+    document.getElementById("item_categories").classList.toggle("nav__item_active");
+    
+    document.getElementById("button_authors").classList.remove("nav-button_active");
+    document.getElementById("item_authors").classList.remove("nav__item_active");
+}
+
+document.getElementById("nav__button_authors").onclick = function() {
+    document.getElementById("button_authors").classList.toggle("nav-button_active");
+    document.getElementById("item_authors").classList.toggle("nav__item_active");
+    
+    document.getElementById("button_categories").classList.remove("nav-button_active");
+    document.getElementById("item_categories").classList.remove("nav__item_active");
 }
