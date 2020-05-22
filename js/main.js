e05bfec80ecec7a0e3499af7a2ec19de7561140a
alex <lazywebstudying@gmail.com>
2020-05-22 19:13:10 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -15,15 +15,9 @@ document.querySelector(".header__menu").onclick = function() {
 document.getElementById("nav__button_categories").onclick = function() {
     document.getElementById("button_categories").classList.toggle("nav-button_active");
     document.getElementById("item_categories").classList.toggle("nav__item_active");
-    
-    document.getElementById("button_authors").classList.remove("nav-button_active");
-    document.getElementById("item_authors").classList.remove("nav__item_active");
 }
 
 document.getElementById("nav__button_authors").onclick = function() {
     document.getElementById("button_authors").classList.toggle("nav-button_active");
     document.getElementById("item_authors").classList.toggle("nav__item_active");
-    
-    document.getElementById("button_categories").classList.remove("nav-button_active");
-    document.getElementById("item_categories").classList.remove("nav__item_active");
 }
