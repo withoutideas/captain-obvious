73ec26293e3d10d17ca2644c729b08815aa9578b
alex <lazywebstudying@gmail.com>
2020-06-08 04:05:23 +0300

    llllll

--- js/main.js
+++ js/main.js
@@ -1,15 +1,60 @@
-const menuToggle = document.querySelector(".header__menu");
+const menuButton = document.getElementById('menu-button');
+const menuList = document.getElementById('menu-list');
+
+const authorsButton = document.getElementById('authors-button');
+const authorsList = document.getElementById('authors-list');
+
+const categoriesButton = document.getElementById('categories-button');
+const categoriesList = document.getElementById('categories-list');
+
+const themeButton = document.getElementById('theme-button');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('nav__button_menu_active');
+    menuList.classList.toggle('nav__list_active');
+});
+
+authorsButton.addEventListener('click', () => {
+    authorsButton.classList.toggle('nav__button_active');
+    authorsList.classList.toggle('nav__list_active');
+});
+
+categoriesButton.addEventListener('click', () => {
+    categoriesButton.classList.toggle('nav__button_active');
+    categoriesList.classList.toggle('nav__list_active');
+});
+
+themeButton.addEventListener('click', () => {
+    themeButton.classList.toggle('nav__button_active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const menuToggle = document.querySelector(".header__menu");
 const nav = document.querySelector(".nav");
 const menuButton = document.querySelector(".menu-button");
 
-menuToggle.addEventListener("click", function() {
+menuToggle.addEventListener("click", () => {
 
     nav.classList.toggle("nav_active");
     menuToggle.classList.toggle("header__menu_active");
 
 });
 
-document.querySelector(".main").addEventListener("click", function() {
+document.querySelector(".main").addEventListener("click", () => {
 
     nav.classList.remove("nav_active");
     menuToggle.classList.remove("header__menu_active");
@@ -33,4 +78,4 @@ const navButton = nav.querySelector(".nav__button_theme");
 navButton.addEventListener("click", function(event) {
 
         navButton.classList.toggle("nav__button_active");
-});
+}); */
