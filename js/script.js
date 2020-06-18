0cafc2e88ec910c018a2d7af44dda412382fa66e
alex <lazywebstudying@gmail.com>
2020-06-18 03:30:49 +0300

    boaring

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-const menuButton = document.getElementById('menu-button');
-const menuList = document.getElementById('menu-list');
+const menuButton = document.getElementById('menu-button'),
+    menuList = document.getElementById('menu-list'),
 
-const authorsButton = document.getElementById('authors-button');
-const authorsList = document.getElementById('authors-list');
-const authorsIcon = document.getElementById('authors-icon');
+    authorsButton = document.getElementById('authors-button'),
+    authorsList = document.getElementById('authors-list'),
+    authorsIcon = document.getElementById('authors-icon'),
 
-const categoriesButton = document.getElementById('categories-button');
-const categoriesList = document.getElementById('categories-list');
-const categoriesIcon = document.getElementById('categories-icon');
+    categoriesButton = document.getElementById('categories-button'),
+    categoriesList = document.getElementById('categories-list'),
+    categoriesIcon = document.getElementById('categories-icon'),
 
-const themeButton = document.getElementById('theme-button');
-const themeIcon = document.getElementById('theme-icon');
+    themeButton = document.getElementById('theme-button'),
+    themeIcon = document.getElementById('theme-icon');
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('nav__button_active');
     menuList.classList.toggle('nav__list_active');
 
     authorsList.classList.remove('nav__list_active');
-    authorsIcon.classList.remove('nav__icon_list_active');
+    authorsIcon.classList.remove('nav__icon_active');
     
     categoriesList.classList.remove('nav__list_active');
-    categoriesIcon.classList.remove('nav__icon_list_active');
+    categoriesIcon.classList.remove('nav__icon_active');
 });
 
 window.addEventListener('scroll', () => {
@@ -28,20 +28,20 @@ window.addEventListener('scroll', () => {
     menuList.classList.remove('nav__list_active');
 
     authorsList.classList.remove('nav__list_active');
-    authorsIcon.classList.remove('nav__icon_list_active');
+    authorsIcon.classList.remove('nav__icon_active');
     
     categoriesList.classList.remove('nav__list_active');
-    categoriesIcon.classList.remove('nav__icon_list_active');
+    categoriesIcon.classList.remove('nav__icon_active');
 });
 
 authorsButton.addEventListener('click', () => {
     authorsList.classList.toggle('nav__list_active');
-    authorsIcon.classList.toggle('nav__icon_list_active');
+    authorsIcon.classList.toggle('nav__icon_active');
 });
 
 categoriesButton.addEventListener('click', () => {
     categoriesList.classList.toggle('nav__list_active');
-    categoriesIcon.classList.toggle('nav__icon_list_active');
+    categoriesIcon.classList.toggle('nav__icon_active');
 });
 
 // check for saved 'darkMode' in localStorage
@@ -69,9 +69,9 @@ if (darkMode === 'enabled') {
 
 // When someone clicks the button
 themeButton.addEventListener('click', () => {
-    themeIcon.classList.toggle('nav__icon_theme_active');
+    themeIcon.classList.toggle('nav__icon_active');
     // get their darkMode setting
-    darkMode = localStorage.getItem('darkMode'); 
+    darkMode = localStorage.getItem('darkMode');
     
     // if it not current enabled, enable it
     if (darkMode !== 'enabled') {
